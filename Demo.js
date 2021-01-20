cbc9dbaa4099c3bf0361764029c7aea43e54ea9a
Rodrigo da Costa Lima <rodrigo.lima@sebsa.com.br>
2021-01-20 18:11:19 -0300

    inicio

---
+++ Demo.js
@@ -0,0 +1,20 @@
+// JavaScript source code
+if (typeof (Demo) === "undefined") { Demo = {}; }
+
+Demo.Conta = {
+
+    OnLoad: function (formContext) {
+    },
+
+    OnChange: function () {
+    },
+
+    OnSave: function () {
+    },
+
+
+
+};
+
+
+
