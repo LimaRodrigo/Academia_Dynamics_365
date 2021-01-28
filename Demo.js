b6f2654b5ae1058508703fd91b638d89f5c742be
Rodrigo da Costa Lima <rodrigo.lima@sebsa.com.br>
2021-01-28 18:14:39 -0300

    function Create e helper

--- Demo.js
+++ Demo.js
@@ -1,41 +1,136 @@
 // JavaScript source code
 if (typeof (Demo) === "undefined") { Demo = {}; }
+// "use strict"
+Demo.Contato = {
 
-Demo.Conta = {
-
+    //Definições de contexto
     formContext: {},
+    saveContext: {},
+    globalContext: Xrm.Utility.getGlobalContext(),
+
+    //Definição dos Campos
+    campoLimiteCredito: "creditlimit",
+    campoConta: "parentcustomerid",
+    campoPreferenciaContato: "preferredcontactmethodcode",
+    campoNome: "firstname",
+    campoAniversario: "birthdate",
+    campoSuspensaoCredito: "creditonhold",
+
+    //Valores Globais
+    valLimiteCredito: null,
+    valConta: null,
+    valPreferenciaContato: null,
+    valNome: null,
+    valAniversario: null,
+    valSuspensaoCredito: null,
+    valOportunidade: {},
 
     OnLoad: function (formContext) {
-        this.formContext = formContext;
+        /**
+         * Chamada de atibição do formContext 
+         * https://docs.microsoft.com/en-us/powerapps/developer/model-driven-apps/clientapi/reference/executioncontext/getformcontext
+         */
+        Demo.Contato.formContext = formContext.getFormContext();
+        Demo.Contato.OnChange();
+        Demo.Contato.RecuperarValoresTipagem();
     },
 
     OnChange: function () {
+        //Adiciona evento onchange em um campo
+        //https://docs.microsoft.com/en-us/powerapps/developer/model-driven-apps/clientapi/reference/attributes/addonchange
+        Demo.Contato.formContext.getAttribute(Demo.Contato.campoLimiteCredito).addOnChange(Demo.Contato.ValidarCriacao);
     },
 
-    OnSave: function () {
+    OnSave: function (saveContext) {
+        Demo.Contato.saveContext = saveContext.getFormContext();
     },
 
+
+
+    /**
+     * Funções de ações em Tela
+     */
+
+    /**
+     * Domenstração de Tipagem do Dynamics
+     */
+    RecuperarValoresTipagem: function () {
+
+        /*Get campos dos tipos */
+
+        //Data
+        console.log("**** Data valores ****");
+        Demo.Contato.valAniversario = Demo.Contato.formContext.getAttribute(Demo.Contato.campoAniversario).getValue();
+        console.log(Demo.Contato.valAniversario);
+
+        //Booleanos 
+        console.log("**** Booleanos valores ****");
+        Demo.Contato.valSuspensaoCredito = Demo.Contato.formContext.getAttribute(Demo.Contato.campoSuspensaoCredito).getValue();
+        console.log(Demo.Contato.valSuspensaoCredito);
+
+        //Numericos (Moeda, Decimais e inteiros)
+        console.log("**** Númericos valores ****");
+        Demo.Contato.valLimiteCredito = Demo.Contato.formContext.getAttribute(Demo.Contato.campoLimiteCredito).getValue();
+        console.log(Demo.Contato.valLimiteCredito);
+
+        //Texto
+        console.log("**** Texto valores ****");
+        Demo.Contato.valNome = Demo.Contato.formContext.getAttribute(Demo.Contato.campoNome).getValue();
+        console.log(Demo.Contato.valNome);
+
+        //Optionset
+        console.log("**** OptionSet valores ****");
+        Demo.Contato.valPreferenciaContato = Demo.Contato.formContext.getAttribute(Demo.Contato.campoPreferenciaContato).getValue();
+        console.log(Demo.Contato.valPreferenciaContato);
+        console.log(Demo.Contato.formContext.getAttribute(Demo.Contato.campoPreferenciaContato).getSelectedOption());
+
+        //Lookup
+        console.log("**** LookUp Valores *****");
+        Demo.Contato.valConta = Demo.Contato.formContext.getAttribute(Demo.Contato.campoConta).getValue();
+        console.log(Demo.Contato.valConta);
+
+    },
+
+    ValidarCriacao: function () {
+        let limite = Demo.Contato.formContext.getAttribute(Demo.Contato.campoLimiteCredito).getValue();
+        if (limite !== null && limite > 5000)
+            Demo.Contato.Create();
+    },
 
     /**
      * Demostração do CRUD 
      */
 
-    Create: function () {
+    Create: async function () {
+        try {
+            let idContato = Demo.Contato.formContext.data.entity.getId().replace("{", "").replace("}", "");
+            let idConta = Demo.Contato.valConta[0].id.replace("{", "").replace("}", "");
+            //Objeto a ser criado
+            var Oportunidade = {};
+            Oportunidade["parentcontactid@odata.bind"] = "/contacts(" + idContato + ")";
+            Oportunidade["parentaccountid@odata.bind"] = "/accounts(" + idConta + ")";
+            Oportunidade.name = "Limite Credito -" + Demo.Contato.valConta[0].name + " - Potencial grande";
+            Oportunidade.purchasetimeframe = 3;
+            Demo.Contato.valOportunidade = Oportunidade;
+            console.log(Oportunidade);
+            //chamada assincrona para criação
+            let result = await Demo.Helper.CriarRegistroAsync("opportunity", Oportunidade);
+            let msg = "Registro " + result.id + " Criado!";
+            Demo.Helper.ExibirAlertaSemConfirmacao(msg, "Sucesso");
+            console.log(result);
+
+        } catch (error) {
+            let retorno = JSON.parse(error.raw);
+            Demo.Helper.ExibirAlertaSemConfirmacao(retorno.message, "Erro");
+        }
+    },
+    Read: async function () {
 
     },
-    Read: function () {
+    Update: async function () {
 
     },
-    Update: function () {
+    Delete: async function () {
 
     },
-    Delete: function () {
-
-    }
-
-
-
 };
-
-
-
---
+++ Helper.js
@@ -0,0 +1,97 @@
+if (typeof (Demo) === "undefined") { Demo = {}; }
+
+Demo.Helper = {
+
+
+    /**
+     * Cria um registro com possiblidade de relacionar registro e/ou criar registros relcionados, na chamada é necessário chamar dentro de um try-catch e utilizar await na frente para aguardar o resultado. 
+     * Mais detalhes no Link: 
+     * https://docs.microsoft.com/en-us/powerapps/developer/model-driven-apps/clientapi/reference/xrm-webapi/createrecord
+     * @param {string} nomeEntidade - Nome lógico da entidade
+     * @param {object} objeto - Objeto a ser criado
+     * @returns {object}
+     */
+    CriarRegistroAsync: function (nomeEntidade, objeto) {
+        return new Promise((resolve, reject) => {
+            Xrm.WebApi.createRecord(nomeEntidade, objeto).then(
+                (result) => { 
+                    resolve(result); 
+                },
+                (e) => { reject(e); }
+            );
+        });
+    },
+     /**
+     * Atualiza um Resgitro, na chamada é necessário chamar dentro de um try-catch e utilizar await na frente para aguardar o resultado. 
+     * Mais detalhes no Link: 
+     * https://docs.microsoft.com/en-us/powerapps/developer/model-driven-apps/clientapi/reference/xrm-webapi/updaterecord
+     * @param {string} nomeEntidade - Nome lógico da entidade
+     * @param {string} id Id do registro a ser atualizado ex.: CFF5AA7F-B426-EB11-BBF3-000D3A887C31
+     * @param {object} objeto - Objeto a ser atualizado
+     * @returns {object}
+     */
+    AtualizarRegistroAsync: function (nomeEntidade, id, objeto) {
+        return new Promise((resolve, reject) => {
+            Xrm.WebApi.updateRecord(nomeEntidade, id, objeto).then(
+                (result) => { resolve(result); },
+                (e) => { reject(e); }
+            );
+        });
+    },
+     /**
+     * Deleta um registro
+     * @param {string} nomeEntidade - Nome lógico da entidade
+     * @param {string} id Id do registro a ser atualizado ex.: CFF5AA7F-B426-EB11-BBF3-000D3A887C31
+     */
+    DeletarRegistroAsync: function (nomeEntidade, id) {
+        return new Promise((resolve, reject) => {
+            Xrm.WebApi.deleteRecord(nomeEntidade, id).then(
+                (result) => { resolve(result); },
+                (e) => { reject(e); }
+            );
+        });
+    },
+     /**
+     * Retorna apenas o registros de acordo com o Id, na chamada é necessário chamar dentro de um try-catch e utilizar await na frente para aguardar o resultado. 
+     * Mais detalhes no Link: 
+     * https://docs.microsoft.com/en-us/powerapps/developer/model-driven-apps/clientapi/reference/xrm-webapi/retrieverecord
+     * @param {string} nomeEntidade - Nome lógico da entidade
+     * @param {string} id - Id do registro a ser recuperado ex.: CFF5AA7F-B426-EB11-BBF3-000D3A887C31
+     * @param {string} optionOdata -  Passa estruta odata select e expand(opcional) ex.:  "?$select=name&$expand=primarycontactid($select=contactid,fullname)"
+     * @returns {object}
+     */
+    RetornarRegistroAsync: function (nomeEntidade, id, optionOdata) {
+        return new Promise((resolve, reject) => {
+            Xrm.WebApi.retrieveRecord(nomeEntidade, id, optionOdata).then(
+                (result) => { resolve(result); },
+                (e) => { reject(e); }
+            );
+        });
+    },
+    /**
+     * Exibe um alerta modal na tela
+     * @param {string} mensagem - Mensagem a ser exibida
+     * @param {string} titulo - titulo do modal
+     * @param {number} largura - tamanho lagura medido em pixels, caso nulo padrão 350
+     * @param {number} altura - tamanho altura medido em pixels,  caso nulo padrão 550
+     */
+    ExibirAlertaSemConfirmacao: function (mensagem, titulo = null, largura = null, altura = null) {
+        let alertStrings = {
+            title: titulo == null ? "" : titulo,
+            confirmButtonLabel: "OK",
+            text: mensagem
+        };
+        let alertOptions = {
+            height: altura == null ? 350 : altura,
+            width: largura == null ? 550 : largura
+        };
+        Xrm.Navigation.openAlertDialog(alertStrings, alertOptions).then(
+            function success(result) {
+                console.log("Sucesso! Alerta exibido!");
+            },
+            function (error) {
+                concole.log(error.message);
+            }
+        );
+    },
+};
